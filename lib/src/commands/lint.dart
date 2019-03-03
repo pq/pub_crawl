e67399db81883725142a4d3fdb6ae1bfbcee6119
pq <pquitslund@gmail.com>
2019-03-02 22:01:52 -0800

    command updates

--- lib/src/commands/lint.dart
+++ lib/src/commands/lint.dart
@@ -1,28 +1,25 @@
 import 'package:analyzer/dart/analysis/analysis_context.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/error/error.dart';
+import 'package:analyzer/src/dart/error/lint_codes.dart';
 import 'package:analyzer/src/generated/engine.dart' show AnalysisOptionsImpl;
 import 'package:analyzer/src/lint/linter.dart';
 import 'package:analyzer/src/lint/registry.dart';
+import 'package:linter/src/rules.dart';
 import 'package:pub_crawl/src/commands/analyze.dart';
 
-class FakeLint extends LintRule implements NodeLintRule {
-  FakeLint()
-      : super(
-            name: 'fake_lint',
-            description: 'fake lint',
-            details: 'not more to say',
-            group: Group.style);
+class LintCommand extends AnalyzeCommand {
+  final List<LintRule> enabledRules = <LintRule>[];
+
+  LintCommand() : super() {
+    argParser.addMultiOption('rules',
+        help: 'A list of lint rules to run. For example: '
+            'avoid_as,annotate_overrides.');
+  }
 
   @override
-  void registerNodeProcessors(NodeLintRegistry registry,
-      [LinterContext context]) {
-    final visitor = new _Visitor(this);
-    registry.addClassDeclaration(this, visitor);
-  }
-}
-
-class LintCommand extends AnalyzeCommand {
+  String get commandVerb => 'Linting';
 
   @override
   String get description => 'lint packages.';
@@ -30,41 +27,28 @@ class LintCommand extends AnalyzeCommand {
   @override
   String get name => 'lint';
 
-  LintCommand() :
-    super() {
-    argParser.addMultiOption('rules',
-        help: 'A list of lint rules to run. For example: '
-            'avoid_as,annotate_overrides.');
-  }
-
+  @override
   void preAnalyze(AnalysisContext context) {
     final optionsImpl = context.analysisOptions as AnalysisOptionsImpl;
     optionsImpl.lint = true;
     optionsImpl.lintRules = enabledRules;
   }
 
-  final List<LintRule> enabledRules = <LintRule>[];
-
+  @override
   Future run() async {
+    registerLintRules();
 
-    /// TMP
-    final lint = FakeLint();
-
-    if (Registry.ruleRegistry[lint.name] == null) {
-      Registry.ruleRegistry.register(lint);
-    }
-
-    var lints = argResults['rules'];
+    final lints = argResults['rules'];
     if (lints == null || lints.isEmpty) {
-      print('No lint rules defined.');
+      print('No lint rules specified.');
       printUsage();
       return;
     }
     if (lints != null && !lints.isEmpty) {
       for (var lint in lints) {
-        var rule = Registry.ruleRegistry[lint];
+        final rule = Registry.ruleRegistry[lint];
         if (rule == null) {
-          print('Unrecognized lint rule: $lint');
+          print('Unrecognized lint rule: $lint.');
           return;
         }
         enabledRules.add(rule);
@@ -73,16 +57,7 @@ class LintCommand extends AnalyzeCommand {
 
     super.run();
   }
-}
-
-class _Visitor extends SimpleAstVisitor<void> {
-  final LintRule rule;
-
-  _Visitor(this.rule);
 
   @override
-  visitClassDeclaration(ClassDeclaration node) {
-    print('... fake lint visiting ${node.name.name}');
-    rule.reportLint(node);
-  }
+  bool showError(AnalysisError error) => error.errorCode is LintCode;
 }
